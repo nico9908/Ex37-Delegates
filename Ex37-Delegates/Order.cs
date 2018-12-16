9e0d2ff5c04556982cb248933a64368a183119e1
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-12-16 10:47:18 +0100

    Tilføjet order class

--- Ex37-Delegates/Order.cs
+++ Ex37-Delegates/Order.cs
@@ -8,5 +8,29 @@ namespace Ex37_Delegates
 {
     class Order
     {
+        public BonusProvider Bonus
+        {
+            get { return Bonus; }
+            set { Bonus = value; }
+        }
+
+        public Product AddProduct(p)
+        {
+            return p;
+        }
+        public double GetValueOfProducts()
+        {
+            return 0;
+        }
+        public double GetBonus()
+        {
+            return 0;
+        }
+        public double GetTotalPrice()
+        {
+            return 0;
+        }
     }
 }
+
+
