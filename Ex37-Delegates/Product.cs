568ba661917217dfda25165e46498545a36cc827
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-12-16 10:38:16 +0100

    Tilføjet indhold til product class

---
+++ Ex37-Delegates/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex37_Delegates
+{
+    class Product
+    {
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public double Value
+        {
+            get { return Value; }
+            set { Value = value; }
+        }
+    }
+}
